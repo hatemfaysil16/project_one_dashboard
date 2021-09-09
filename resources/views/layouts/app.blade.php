9d8bd9c2d5c7cb20b43742f80ae11717898b3d27
hatemfaysil <hatemfasyil16@gmail.com>
2021-09-09 08:45:35 +0200

    foure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title> Muhammed Essa </title>
+    <title> hatem faysil </title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
